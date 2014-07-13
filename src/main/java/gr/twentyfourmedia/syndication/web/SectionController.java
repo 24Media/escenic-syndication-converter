0667066100719a9fa8e93b5242438008f32d94f4
blixabargeld <nikolaos.i.papadopoulos@gmail.com>
2014-07-13 13:26:10 +0300

    SectionController Now Works With Moxy

--- src/main/java/gr/twentyfourmedia/syndication/web/SectionController.java
+++ src/main/java/gr/twentyfourmedia/syndication/web/SectionController.java
@@ -4,9 +4,15 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
@@ -14,8 +20,8 @@ import gr.twentyfourmedia.syndication.model.Escenic;
 import gr.twentyfourmedia.syndication.model.Section;
 import gr.twentyfourmedia.syndication.service.SectionService;
 
+import org.eclipse.persistence.oxm.CharacterEscapeHandler;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +29,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/section")
 public class SectionController {
-	
-	@Autowired
-	private Jaxb2Marshaller marshaller;
 	
 	@Autowired
 	private SectionService sectionService;	
@@ -45,13 +48,28 @@ public class SectionController {
 		try {
 			
 			outputStream = new FileOutputStream(new File(path));
-			
 			StreamResult result = new StreamResult(outputStream);
-			
-			marshaller.marshal(sections, result);	
+			JAXBContext jaxbContext = JAXBContext.newInstance(Escenic.class);
+            Marshaller marshaller = jaxbContext.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            
+            marshaller.setProperty("com.sun.xml.internal.bind.characterEscapeHandler",
+                new CharacterEscapeHandler() {
+                    @Override
+                    public void escape(char[] ch, int start, int length, boolean isAttVal, Writer writer) throws IOException {
+                    	writer.write(ch, start, length);
+                    }
+                }
+            );
+            
+            marshaller.marshal(sections, result);
 		} 
 		catch(FileNotFoundException exception) {
 			
+			exception.printStackTrace();
+		}
+		catch (JAXBException exception) {
+
 			exception.printStackTrace();
 		}
 		
@@ -66,10 +84,14 @@ public class SectionController {
 		FileInputStream inputStream;
 		
 		try {
+			
 			inputStream = new FileInputStream(new File(path));
 			StreamSource source = new StreamSource(inputStream);
 			
-			Escenic escenic = (Escenic) marshaller.unmarshal(source);
+			Escenic escenic = null;
+			JAXBContext	jaxbContext = JAXBContext.newInstance(Escenic.class);
+	        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+	        escenic = (Escenic) unmarshaller.unmarshal(source);	
 			
 			for(Section e : escenic.getSectionList()) {
 
@@ -79,7 +101,11 @@ public class SectionController {
 		catch (FileNotFoundException exception) {
 			
 			exception.printStackTrace();
-		} 
+		}
+		catch (JAXBException exception) {
+			
+			exception.printStackTrace();
+		}
 		
 		return "/home";
 	}
